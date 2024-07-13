470b233135c5f8a9d51f38775848237039a02251
jassim mohamed <159074105+jasbx@users.noreply.github.com>
2024-07-13 14:03:43 +0300

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
                             {openCart &&<Cart/>}
                         </div>
                   
-                        <button onClick={()=>setMenu(!openMenu)} data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                        <button onClick={()=>setMenu(!openMenu)} data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex space-x-10 md:space-x-0 items-center p-2 ml-1 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                             
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                            
