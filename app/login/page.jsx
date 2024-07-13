4aed3b113c4cdc17926590656ec4aece98c81387
jassim mohamed <159074105+jasbx@users.noreply.github.com>
2024-07-13 13:10:30 +0300

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -11,7 +11,7 @@ const page = () => {
     <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
       <img
         alt=""
-        src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        src="/bg5.jpg"
         className="absolute inset-0 h-full w-full object-cover"
       />
     </aside>
