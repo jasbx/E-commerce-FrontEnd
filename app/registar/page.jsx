542c549beb9af37a7b3504522aab5725f956657a
jassim mohamed <159074105+jasbx@users.noreply.github.com>
2024-07-13 14:23:01 +0300

    Update page.jsx

--- app/registar/page.jsx
+++ app/registar/page.jsx
@@ -21,7 +21,7 @@ const page = () => {
         </a>
 
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to Squid 🦑
+          Welcome to Our Website 
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
